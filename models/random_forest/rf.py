2e201e46f718c140368a36df859791f5ab09811a
Florian Paul <florian.paul@hs-augsburg.de>
2024-05-13 11:54:01 +0200

    comments

--- models/random_forest/rf.py
+++ models/random_forest/rf.py
@@ -1,15 +1,14 @@
 import pandas as pd
-import time
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.model_selection import train_test_split, TimeSeriesSplit
-from sklearn.metrics import (
-    mean_absolute_error,
-    root_mean_squared_error,
-    mean_absolute_percentage_error,
-)
 
 
 def prepare_data(data):
+    """
+    Prepares the data for running the model
+
+    :param data: Panda DataFrame with date and occupancy
+    :return: expand data with doy, dow, month, year
+    """
     data.set_index("date", inplace=True)
     data["day_of_year"] = data.index.dayofyear
     data["day_of_week"] = data.index.dayofweek
@@ -20,15 +19,21 @@ def prepare_data(data):
 
 class Rf:
     def __init__(self, data: pd.DataFrame, predict_range: int, rf_params: dict):
+        # get the params passed by the wrapper script
         self.data = prepare_data(data)
         self.predict_range = predict_range
         self.reset_params()
         self.set_params(rf_params)
 
+        # split the prepared data for the model
         self.x = data[["day_of_year", "day_of_week", "month", "year"]]
         self.y = data["occupancy"]
 
     def predict(self):
+        """
+        Predicts the occupancy for the specified time range
+        :return: Pandas DataFrame with predicted occupancy for each date in the time range
+        """
         rf_model = RandomForestRegressor()
         rf_model.fit(self.x, self.y)
         latest_date = self.data.index.max()
@@ -44,14 +49,16 @@ class Rf:
         return future_features["predictions"]
 
     def put_dataset(self, dataset):
+        """
+        Used to change the dataset for the model
+        :param dataset: Pandas Dataframe with date and occupancy
+        """
         self.data = prepare_data(data=dataset)
         self.x = self.data[["day_of_year", "day_of_week", "month", "year"]]
         self.y = self.data["occupancy"]
-        # Hier könnten weitere Vorbereitungen für das Dataset erfolgen
 
     def set_daterange(self, daterange):
         self.daterange = daterange
-        # Hier könnten weitere Vorbereitungen für die Daterange erfolgen
 
     def get_params(self):
         return self.rf_regressor_params
